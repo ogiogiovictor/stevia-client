259046391cf141dd8d285d4bb6e3e44d109a16b0
Daniel Ufeli <danielufeli@yahoo.com>
2020-02-04 21:34:53 +0100

    token issue

--- src/Utils/setAuthToken.js
+++ src/Utils/setAuthToken.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const setAuthToken = token => {
-    if(!token) {
+    if(token) {
         axios.defaults.headers.common['AuthToken'] = token;
     } else {
         delete axios.defaults.headers.common['AuthToken'];
