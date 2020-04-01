66f5ad5cb0d7c2b66cf8e3dc2e867818c50413f3
Daniel Ufeli <danielufeli@yahoo.com>
2020-04-01 18:13:47 +0100

    change select company

--- src/actions/jobs.js
+++ src/actions/jobs.js
@@ -71,6 +71,7 @@ export const addCompany = formData => async dispatch => {
     dispatch(setAlert(res.data.msg, 'success'));
     dispatch(getCompany());
     dispatch(getCompany());
+    dispatch(getCompany());
       // setTimeout(() => {
       //   dispatch(window.location.reload())
       // }, 3000);
@@ -78,6 +79,7 @@ export const addCompany = formData => async dispatch => {
       type: ADD_COMPANY,
       payload: res.data.msg
     });
+    dispatch(getCompany());
   } catch (error) {
     dispatch({
       type: JOBS_ERROR,
